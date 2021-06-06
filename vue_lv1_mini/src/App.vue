5bd5ab98a9ac594652235e6f7346df0f916c31ea
Abangpa1ace <dyek72731@gmail.com>
2021-06-07 00:05:36 +0900

    Vue 미니프로젝트 실습중

---
+++ vue_lv1_mini/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="main">
+    <!-- {{ str }}
+    <AppHeader :text="head" @send="changeHead"></AppHeader> -->
+    <Form />
+  </div>
+</template>
+
+<script>
+// import AppHeader from './components/AppHeader';
+import Form from './components/Form';
+
+export default {
+  components: {
+    // AppHeader,
+    Form,
+  },
+  data() {
+    return {
+      head: 'Header TExt!'
+    }
+  },
+  methods: {
+    changeHead(text) {
+      this.head = text;
+    }
+  },
+}
+</script>
+
+<style lang="css">
+
+body {
+  padding: 80px;
+  background: #f4f4f4;
+}
+
+</style>
---
+++ vue_lv1_mini/src/components/Form.vue
@@ -0,0 +1,22 @@
+<template>
+  <form action="">
+    <div>
+      <label for="userId">id: </label>
+      <input name="userId" type="text">
+    </div>
+    <div>
+      <label for="userPw">pw: </label>
+      <input name="userPw" type="password">
+    </div>
+  </form>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
