7a2ceab342187a03502624cc72ff58fce9fe87f7
Abangpa1ace <dyek72731@gmail.com>
2021-06-06 23:55:26 +0900

    Vue 미니프로젝트 실습중

--- vue_lv1_toy/src/App.vue
+++ vue_lv1_toy/src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{ str }}
+    <AppHeader 
+      :text="head"
+      @send="changeHead">
+    </AppHeader>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    AppHeader,
+  },
+  data() {
+    return {
+      head: 'Header TExt!'
+    }
+  },
+  methods: {
+    changeHead(text) {
+      this.head = text;
+    }
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+
 </style>
---
+++ vue_lv1_toy/src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>{{ text }}</h1>
+    <button @click="sendMsg">send</button>
+  </header>
+</template>
+
+<script>
+  export default {
+    props: {
+      text: {
+        type: String,
+        default: "",
+      }
+    },
+    methods: {
+      sendMsg() {
+        this.$emit('send', "New Header!!")
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
