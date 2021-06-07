91ea513b8c1d3abe34e1fb975e1f9473d910be4b
taeng <dyek72731@gmail.com>
2021-06-07 16:54:54 +0900

    Vue 폼구현

--- vue_lv1_mini/src/components/Form.vue
+++ vue_lv1_mini/src/components/Form.vue
@@ -1,20 +1,45 @@
 <template>
-  <form action="">
+  <form @submit.prevent="submitInfo">
     <div>
       <label for="userId">id: </label>
-      <input name="userId" type="text">
+      <input 
+        name="userId" 
+        type="text"
+        v-model="id">
     </div>
     <div>
       <label for="userPw">pw: </label>
-      <input name="userPw" type="password">
+      <input 
+        name="userPw" 
+        type="password"
+        v-model="pw">
     </div>
+    <button 
+      type="submit"
+      >login</button>
   </form>
 </template>
 
 <script>
-  export default {
-    
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      id: '',
+      pw: '',
+    }
+  },
+  methods: {
+    submitInfo() {
+      // e.preventDefault();
+      const url = 'https://jsonplaceholder.typicode.com/users'
+      axios.post(url, {username: this.id, password: this.pw})
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    }
   }
+}
 </script>
 
 <style lang="scss" scoped>
