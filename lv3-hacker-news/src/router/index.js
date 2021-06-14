d743dca6d1aa35d0848b4d65607d2687ad181778
Abangpa1ace <dyek72731@gmail.com>
2021-06-14 21:53:32 +0900

    Vue App 제작중

---
+++ lv3-hacker-news/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import NewsView from '../views/NewsView.vue'
+import JobsView from '../views/JobsView.vue'
+import AskView from '../views/AskView.vue'
+
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      redirect: '/news'
+    },
+    {
+      path: '/news',
+      component: NewsView
+    },
+    {
+      path: '/jobs',
+      component: JobsView
+    },
+    {
+      path: '/ask',
+      component: AskView
+    },
+    // {
+    //   path: '',
+    //   component: '',
+    // },
+    // {
+    //   path: '',
+    //   component: '',
+    // },
+  ]
+})
+
+export default router;
